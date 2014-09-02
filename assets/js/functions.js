dd67312d2cff32a9baad2760646191b816e25b42
wushuyi <wushuyi541@gmail.com>
2014-09-02 17:03:25 +0800

    进度保存

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -46,6 +46,53 @@
             right: '-100px',
             opacity: '0'
         });
+
+        $cache.bt = $('#bt');
+        $cache.tryBtn = $('.try_submit', $cache.bt);
+        $cache.tryName = $('.try_name', $cache.bt);
+        $cache.tryPhone = $('.try_phone', $cache.bt);
+
+        $cache.tryBtn.data('lock', false);
+        $cache.tryBtn.on('click', function(e){
+            if($cache.tryBtn.data('lock')){
+                return false;
+            }
+            var name = $cache.tryName.val();
+            var phone = $cache.tryPhone.val();
+            if($.trim(name).length == 0 || name == '填写姓名')
+            {
+                alert('请输入姓名!')	;
+                $cache.tryName.focus();
+                return false;
+            }
+            if(!/^1[3|4|5|6|7|8|9][0-9]\d{8}$/.test(phone))
+            {
+                alert('手机号码输入有误!');
+                $cache.tryPhone.focus();
+                return;
+            }
+            $cache.tryBtn.data('lock', true);
+            var cccdata = {
+                car:7,
+                dealer:'1536',
+                email:'',
+                mobile: phone,
+                name: name,
+                pro:'1',
+                purchase_intention:'三个月内'
+            };
+            $.getJSON('http://www.chevrolet.com.cn/brandsite/ccc/try_order_sync.ashx?callback=?', cccdata, function(data){
+                if (data.result == "success") {
+                    if (data.jsonResponse == 0) {
+                        alert("提交失败");
+                    } else if (data.jsonResponse == 1) {
+                        alert('提交成功');
+                    }
+                } else {
+                    alert("提交失败");
+                }
+            });
+        });
     });
      $(window).load(function() {
          $cache.loading.hide();
